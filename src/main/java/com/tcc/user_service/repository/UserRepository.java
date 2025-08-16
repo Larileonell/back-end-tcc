c18cc9418c7f0d694697b66f5a070b346f4d391d
Larissa leonel <larissaleonel98@gmail.com>
2025-08-16 06:49:57 -0300

    creating the repository

---
+++ src/main/java/com/tcc/user_service/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.tcc.user_service.repository;
+
+import com.tcc.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
