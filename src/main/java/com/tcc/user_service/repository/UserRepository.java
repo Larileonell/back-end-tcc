d09d5c21a953a6b77998ae387547cb09a11b5455
Larissa leonel <larissaleonel98@gmail.com>
2025-08-18 18:17:16 -0300

    add changes

--- src/main/java/com/tcc/user_service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tcc.user_service.repository;
-
-import com.tcc.user_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
